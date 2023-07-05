4a576174901b2bc1a3ecfc70cfbee1bc9ca482b0
Farkas Bence <farkas.bence27@gmail.com>
2023-07-05 12:43:53 +0200

    import react router components to app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import { Route, Routes, Link } from "react-router-dom";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
-
-console.log(import.meta.env.VITE_FIREBASE_API_KEY)
+  console.log(import.meta.env.VITE_FIREBASE_API_KEY);
   return (
     <>
-     <h1 className="font-bold text-3xl">Hello World!</h1>
+      <h1 className="font-bold text-3xl">Hello World!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
