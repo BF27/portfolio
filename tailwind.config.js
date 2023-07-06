54e280db6532a679e8624cec1a1a7d84888d4511
Farkas Bence <farkas.bence27@gmail.com>
2023-07-06 14:54:49 +0200

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    colors: {
+      "light-bg": "#D9D9D9",
+      blue: "#3430FA",
+    },
+    fontFamily: {
+      fontHeading: ["Space Grotesk", "sans-serif"],
+      fontBody: ["Roboto", "sans-serif"],
+    },
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
