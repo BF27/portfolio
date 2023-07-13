5f9c4ffe18a657888f39cccdb2830387b3c5144b
Farkas Bence <farkas.bence27@gmail.com>
2023-07-13 15:23:26 +0200

    add about page

--- src/components/TopBar/components/HeaderTitle/index.jsx
+++ src/components/TopBar/components/HeaderTitle/index.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const HeaderTitle = () => {
   return (
     <div className="border-x-4 md:border-0 border-blue w-2/4 md:w-2/6 flex items-center justify-center text-lg sm:text-3xl">
-      <h1>Bence Farkas</h1>
+      <Link to="/">
+        <h1>Bence Farkas</h1>
+      </Link>
     </div>
   );
 };
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,13 +1,48 @@
 import { useEffect } from "react";
+import profilePhoto from "../../assets/profile.svg";
 import Header from "../../components/Header";
 
 const About = ({ changePage }) => {
-  
   useEffect(() => {
     changePage("about");
   }, []);
 
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className="mb-20 pt-8 w-full flex justify-center">
+        <div className="container mx-auto flex justify-center">
+          <div className="w-4/6 flex items-end">
+            <div className=" lg:w-2/3 text-blue">
+              <h3 className="text-3xl w-full mb-6">Hi!</h3>
+              <p>
+                Hi! I am a results-driven frontend developer with a deep passion
+                for creating captivating user experiences. With [number] years
+                of experience, I specialize in crafting visually stunning
+                websites that are not only aesthetically pleasing but also
+                highly functional and user-friendly. Proficient in HTML, CSS,
+                and JavaScript, I excel at transforming designs into responsive,
+                pixel-perfect interfaces.
+              </p>
+              <p>
+                Collaboration is at the heart of my approach. I enjoy working
+                closely with designers, backend developers, and stakeholders to
+                ensure seamless integration of design and functionality. By
+                leveraging my strong communication skills and keen attention to
+                detail, I consistently deliver projects that exceed
+                expectations.
+              </p>
+            </div>
+            <img
+              src={profilePhoto}
+              alt="Profile photo"
+              className="hidden lg:block w-1/3"
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default About;
