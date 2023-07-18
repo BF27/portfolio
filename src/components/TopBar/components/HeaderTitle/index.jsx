28bee3261819295c02b349b66549527f35c33c0d
Farkas Bence <farkas.bence27@gmail.com>
2023-07-18 15:46:04 +0200

    add function that toggle the mobile Nav if title clicked

--- src/components/TopBar/components/HeaderTitle/index.jsx
+++ src/components/TopBar/components/HeaderTitle/index.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+import { toggleMobileNavContext, mobileNavContext } from "../../../../contexts/toggleMobileNavContext";
 import { Link } from "react-router-dom";
 
 const HeaderTitle = () => {
+  const toggleMobileNav = useContext(toggleMobileNavContext);
+  const mobileNav = useContext(mobileNavContext);
+  const click = () => {
+    if (mobileNav) {
+      toggleMobileNav();
+    }
+  }
   return (
     <div className="border-x-4 md:border-0 border-blue w-2/4 md:w-2/6 flex items-center justify-center text-lg sm:text-3xl">
-      <Link to="/">
+      <Link to="/" onClick={click}>
         <h1>Bence Farkas</h1>
       </Link>
     </div>
